87248c82ff3af640eef5bd8891532e9e5c78e868
Wes Sleeman <weston@wsleeman.com>
2018-12-01 14:41:00 +0000

    Added build

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
     public GameObject[] SpawnPoints;
     public float Speed = 3.0f;
@@ -16,6 +17,9 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
+        if (!isLocalPlayer)
+            return;
+        
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * StrafeSpeed;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * Speed;
 
