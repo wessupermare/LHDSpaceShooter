40ec95f30d75fe954c3d4c7a43f878410a4d3a31
Wes Sleeman <weston@wsleeman.com>
2018-12-01 15:56:43 +0000

    Build

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,25 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
-public class Health : MonoBehaviour
+public class Health : NetworkBehaviour
 {
     public RectTransform HealthBar;
 
     public const int MaxHealth = 100;
+
+    [SyncVar(hook = "OnChangeHealth")]
     public int CurrentHealth = MaxHealth;
 
     public void TakeDamage(int amount)
     {
+        if (!isServer)
+            return;
+
         CurrentHealth -= amount;
-        
+
         if (CurrentHealth <= 0)
         {
             CurrentHealth = 0;
             Debug.Log("Dead!");
         }
+    }
 
-        HealthBar.sizeDelta = new Vector2(CurrentHealth, HealthBar.sizeDelta.y);
+    void OnChangeHealth(int currentHealth)
+    {
+        HealthBar.sizeDelta = new Vector2(currentHealth, HealthBar.sizeDelta.y);
     }
 }
